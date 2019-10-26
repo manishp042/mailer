e5753b8f4a02c3bc5a70c0dac9592f53ac21354a
Manish Pandey <manish22sepp@gmai.com>
2019-10-26 21:50:34 +0530

    command job and queue work

---
+++ app/Jobs/SendEmailToUsers.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Mail;
+use App\Mail\SendMail;
+use App\User;
+
+class SendEmailToUsers implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+   
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+         $user = New User();
+         $users = $user->all();
+         foreach($users as $u){
+              Mail::to($u->email)->send(New SendMail());
+         }
+    }
+}
